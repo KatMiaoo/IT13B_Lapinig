2a991fb335b55b96173944df38c540ff6c350d08
KIM <kaisermeatriderr@gmail.com>
2025-02-13 12:26:38 +0800

    Added
    Added HelloWorld class

---
+++ Lapinig_IT13B_Activities/src/Prelim/Lab_Act1_Artihmetics.java
@@ -0,0 +1,18 @@
+
+package Prelim;
+
+public class Lab_Act1_Artihmetics {
+    public static void main (String [] args){
+        
+        int x = 10;
+        int y = 4;
+        int z = 6;
+        
+        System.out.println(x + " * " + y + " + " + z + " = " + (x * y + z));
+        System.out.println( "(" + x + " - " + y + ")" + " % " + z + " = " + ((x - y) % z));
+        System.out.println( "(" + x + " + " + y + " + " + z + ")" + " / " + " 3 " + " = " + ((x + y + z)/3));
+        System.out.println( x + " * " + z + " - " + "(" + y + " * " + y + ")" + " = " + (x * z - (y * y)));
+
+    }
+}
+
