73297bea6a6f78ecc883073c3415f8147bcbf098
jasiel333 <kaisermeatriderr@gmail.com>
2025-02-24 11:54:06 +0800

    Added Act 2

--- Lapinig_IT13B_Activities/src/Prelim/Lab_Act2_greatest_number.java
+++ Lapinig_IT13B_Activities/src/Prelim/Lab_Act2_greatest_number.java
@@ -1,15 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Prelim;
 
 import java.util.Scanner;
-
-/**
- *
- * @author User
- */
 public class Lab_Act2_greatest_number {
     public static void main (String[] args){
         Scanner input=new Scanner (System.in);
