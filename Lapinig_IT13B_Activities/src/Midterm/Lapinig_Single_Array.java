71d39eac9c6f88c0e8ca943423783f0092b09ed3
jasiel333 <jasieldumana951@gmail.com>
2025-03-24 10:51:24 +0800

    Added

---
+++ Lapinig_IT13B_Activities/src/Midterm/Lapinig_Single_Array.java
@@ -0,0 +1,14 @@
+package Midterm;
+
+
+public class Lapinig_Single_Array {
+    
+    public static void main(String[]args) {
+        char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G',};
+        
+        for (char x: letters) {
+            System.out.print (x + " , ");
+        }
+    }
+    
+}
